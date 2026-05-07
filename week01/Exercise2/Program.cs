ab9ef1534762572266d765e69f03e876704958bf
FacundoTapia94 <ftapia1@byupathway.edu>
2026-05-07 17:10:11 -0300

    COmpleted the activity

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Hello, Please enter your grade persentage: ");
+        string grade = Console.ReadLine();
+        int gradeInt = int.Parse(grade);
+        
+        string letter = "";
+
+        if (gradeInt >= 90)
+            {
+                letter = "A"; 
+            }
+        
+        else if (gradeInt >= 80)
+            {
+                letter = "B";
+            }
+        
+        else if (gradeInt >= 70)
+            {
+                letter = "C";
+            }
+        
+        else if (gradeInt >= 60)
+            {
+                letter = "D";
+            }
+        
+        else 
+            {
+                letter = "F";
+            }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (gradeInt >= 70)
+        {
+            Console.WriteLine("Congratulations, you have passed the course!");
+        }
+
+        else 
+        {
+            Console.WriteLine("Sorry, You have not passed the course. I encourage you to try it again.");
+        }
+
     }
 }
