cdbe176acf0c41598f5f579764442ee5d4860def
FacundoTapia94 <ftapia1@byupathway.edu>
2026-05-07 18:16:42 -0300

    Exercise 3 completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Welcome to the guess the number game!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101); 
+
+        int guessNumber = -1;
+
+        while (guessNumber != number)
+        {
+            Console.Write("What is your guess? ");
+            guessNumber = int.Parse(Console.ReadLine());
+
+            if (guessNumber > number)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (guessNumber < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+
+            {
+            Console.WriteLine("You guessed it!");
+            }
+
+        }
+
+        
+        
+
+
     }
 }
